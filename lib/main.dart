d3e7fd560afb0bff9a640fd46d26791a373e7e6b
robinpfister <119607482+robinpfister@users.noreply.github.com>
2023-10-11 22:52:04 +0200

    add steering wheel

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'dart:math' as math;
 
 void main() => runApp(const MyApp());
 
@@ -86,15 +89,79 @@ class ControlPage extends StatelessWidget {
         appBar: AppBar(
             title: const Text('CONTROL')
         ),
-        body: Align(
-            alignment: Alignment.topLeft,
-            child: OutlinedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('back')
-            )
-        )
+        body: Stack(
+                children: <Widget> [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: OutlinedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('back')
+                    )
+                  ),
+                  const SteeringWheel()
+                ]
+        ),
     );
+  }
+}
+
+class SteeringWheel extends StatefulWidget {
+  const SteeringWheel({Key? key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() => _SteeringAngle();
+}
+
+class _SteeringAngle extends State<SteeringWheel> {
+  double value = 0;
+  @override
+  Widget build(BuildContext context) {
+      return Stack(
+        children: <Widget> [
+          Center(
+              child: Transform.rotate(
+                  angle: (value * (math.pi/180)),
+                  child: Image.asset('assets/images/steeringWheel.png', scale: 0.5)
+              )
+          ),
+          Center(
+              child: SleekCircularSlider(
+                  min: -60,
+                  max: 60,
+                  initialValue: 0,
+                  innerWidget: (double value) {
+                    return Center(
+                        child: Transform.rotate(
+                            angle: (value * (math.pi/180)),
+                            child: const FaIcon(FontAwesomeIcons.circle, size:50)
+                        )
+                    );
+                  },
+                  appearance: CircularSliderAppearance(
+                      size: 235,
+                      startAngle: 210,
+                      angleRange: 120,
+                      customColors: CustomSliderColors(
+                        trackColor: Colors.black,
+                        progressBarColors: null,
+                        progressBarColor: Colors.black,
+                        dotColor: Colors.red
+                      ),
+                      customWidths: CustomSliderWidths(
+                        progressBarWidth: 5,
+                        trackWidth: 5,
+                        handlerSize: 10
+                      )
+                  ),
+                  onChange: (double value) {
+                    setState(() {
+                      this.value = value;
+                    });
+                  }
+              )
+          )
+        ]
+      );
   }
 }
