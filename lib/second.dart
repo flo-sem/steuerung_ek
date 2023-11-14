98370c893dcc142861b652f798a65cddcd5776ff
96flose <96flose@gmail.com>
2023-11-14 16:56:43 +0100

    removed double button

--- lib/second.dart
+++ lib/second.dart
@@ -13,66 +13,44 @@ class SecondScreen extends StatefulWidget {
 class _SecondScreen extends State<SecondScreen> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<StateManager>(
-        builder: (context, stateManager, child)
-        {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Bluetooth Characteristic Value'),
-      ),
-      backgroundColor: stateManager.backgroundColor,
-      body: Center(
-        // Center the content
-        child: Column(
-          mainAxisAlignment:
-          MainAxisAlignment.center, // Center the column itself
-          children: <Widget>[
-            StreamBuilder<List<int>>(
-              stream: stateManager.bluetoothProvider.charValueStream,
-              builder:
-                  (BuildContext context, AsyncSnapshot<List<int>> snapshot) {
-                if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                }
-                switch (snapshot.connectionState) {
-                  case ConnectionState.none:
-                    return Text('Not connected to a stream...');
-                  case ConnectionState.waiting:
-                    return Text('Awaiting values...');
-                  case ConnectionState.active:
-                  case ConnectionState.done:
-                    String valueAsString = snapshot.data
-                        ?.map((e) => e.toRadixString(16).padLeft(2, '0'))
-                        .join(':') ??
-                        'No Data';
-                    return Text('Value: $valueAsString');
-                  default:
-                    return CircularProgressIndicator();
-                }
-              },
-            ),
-            SizedBox(height: 20), // Add some spacing
-            ElevatedButton(
-              onPressed: () {
-                List<int> input = [
-                  0x48,
-                  0x65,
-                  0x6c,
-                  0x6c,
-                  0x6f,
-                  0x20,
-                  0x77,
-                  0x6f,
-                  0x72,
-                  0x6c,
-                  0x64
-                ];
-                ble_info().BLE_WriteCharateristics(input);
-              },
-              child: Text('Send HelloWorld'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
+    return Consumer<StateManager>(builder: (context, stateManager, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Bluetooth Characteristic Value'),
+        ),
+        backgroundColor: stateManager.backgroundColor,
+        body: Center(
+          // Center the content
+          child: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Center the column itself
+            children: <Widget>[
+              StreamBuilder<List<int>>(
+                stream: stateManager.bluetoothProvider.charValueStream,
+                builder:
+                    (BuildContext context, AsyncSnapshot<List<int>> snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  }
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                      return Text('Not connected to a stream...');
+                    case ConnectionState.waiting:
+                      return Text('Awaiting values...');
+                    case ConnectionState.active:
+                    case ConnectionState.done:
+                      String valueAsString = snapshot.data
+                              ?.map((e) => e.toRadixString(16).padLeft(2, '0'))
+                              .join(':') ??
+                          'No Data';
+                      return Text('Value: $valueAsString');
+                    default:
+                      return CircularProgressIndicator();
+                  }
+                },
+              ),
+              SizedBox(height: 20), // Add some spacing
+              ElevatedButton(
                 onPressed: () {
                   List<int> input = [
                     0x48,
