8b0db2a7259d9c808743b1235a6f5e0afaa5bba2
Marco <marcoleon.rapp@gmail.com>
2023-11-14 12:02:47 +0100

    added settings section to set minimum send delay

--- lib/main.dart
+++ lib/main.dart
@@ -92,8 +92,8 @@ class _StartPage extends State<StartPage> {
                       stateManager.setSteeringAngle(0.0);
                       Navigator.push(context,
                           MaterialPageRoute(builder: (context) {
-                        return const ControlPage(title: 'SettingPage');
-                        //return const SettingsPage();
+                        //return const ControlPage(title: 'SettingPage');
+                        return const SettingsPage();
                       }));
                     },
                     child: const Text('Settings'))),
@@ -183,7 +183,43 @@ class _SettingsPage extends State<SettingsPage> {
                         child: Text('Blue'),
                       ),
                     ],
-                  )
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(children: [
+                      Row(
+                        children: [
+                          Text(
+                            'Min. send Interval',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Spacer()
+                        ],
+                      ),
+
+                      //Text input for minimum ble sending interval
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        onChanged: (newValue) {
+                          setState(() {
+                            //Update the numberValue when the text field changes
+                            int num = int.tryParse(newValue) ?? 20;
+                            //print(num);
+                            stateManager.minimumSendDelay = num;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          labelText: '${stateManager.minimumSendDelay} ms',
+                          border: OutlineInputBorder(),
+                          suffixText: 'ms', // Suffix added here
+                        ),
+                      ),
+                    ]),
+                  ),
                 ],
               )
             ],
