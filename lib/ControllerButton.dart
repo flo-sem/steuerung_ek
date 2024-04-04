5ccaec6cda20f2d19a2169fd9245011d334df7f2
robinpfister <119607482+robinpfister@users.noreply.github.com>
2024-04-04 12:02:34 +0200

    add new controller display

--- lib/ControllerButton.dart
+++ lib/ControllerButton.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:steuerung_ek/state_manager.dart';
 import 'package:provider/provider.dart';
+import 'package:steuerung_ek/ControllerDisplay.dart';
 
 class ControllerButton extends StatefulWidget {
   const ControllerButton({Key? key}) : super(key: key);
@@ -14,7 +15,7 @@ class _ControllerButtonState extends State<ControllerButton> {
     return Consumer<StateManager>(
         builder: (context, stateManager, child) {
           return GestureDetector(
-              onTapDown: (_) => stateManager.toggleControllerConnectionState(),
+              onTapDown: (_) => onPressed(),
               onTapUp: (_) => stateManager.resetControllerButtonState(),
               child: Transform.scale(
                   scale: 1 - (0.2 * stateManager.controllerButtonState),
@@ -24,5 +25,17 @@ class _ControllerButtonState extends State<ControllerButton> {
           );
         }
     );
+  }
+
+  void onPressed() {
+    var stateManager = Provider.of<StateManager>(context, listen: false);
+    stateManager.toggleControllerConnectionState();
+    if(stateManager.controllerConnectionState == 1) {
+      Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => ControllerDisplay()));
+    }
+    else {
+      Navigator.of(context).pop();
+    }
   }
 }
